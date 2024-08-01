aab2e611c383a786f6447b335cfdffe61bb6c054
Dhison <dhisonp@gmail.com>
2024-08-01 00:02:43 -0400

    chore: removed unused comments in fetch-bio.ts

--- src/pages/api/fetch-bio.ts
+++ src/pages/api/fetch-bio.ts
@@ -1,14 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import plAxios from '@/app/services/axios';
+import plAxios from '@/pages/services/axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const url = '/api/globals/bio-global?locale=undefined&draft=false&depth=1';
-
-    // const headers = {
-    //   'x-api-key': process.env.PAYLOAD_SECRET,
-    // };
-
     const response = await plAxios.get(url);
     res.status(200).json(response.data);
   } catch (err) {
