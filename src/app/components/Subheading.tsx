b16a150e7aeed60d0f30967b5078eb19a07bcfed
Dhison <dhisonp@gmail.com>
2025-05-16 13:49:31 -0400

    reformat: apply prettier to root dir

--- src/app/components/Subheading.tsx
+++ src/app/components/Subheading.tsx
@@ -1,18 +1,12 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 interface Props {
   children?: React.ReactNode;
   className?: string;
 }
 
-const Subheading: FC<Props> = ({ children, className = "" }) => {
-  return (
-    <div
-      className={`text-base text-zinc-600 dark:text-zinc-400 self-start ${className}`}
-    >
-      {children}
-    </div>
-  );
+const Subheading: FC<Props> = ({ children, className = '' }) => {
+  return <div className={`text-base text-zinc-600 dark:text-zinc-400 self-start ${className}`}>{children}</div>;
 };
 
 export default Subheading;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,29 @@
-import "./styles/globals.css";
-import type { Metadata } from "next";
-import { IBM_Plex_Sans, IBM_Plex_Serif } from "next/font/google";
+import './styles/globals.css';
+import type { Metadata } from 'next';
+import { IBM_Plex_Sans, IBM_Plex_Serif } from 'next/font/google';
 
 const ibmPlexSans = IBM_Plex_Sans({
-  weight: "300",
-  display: "swap",
-  subsets: ["latin"],
-  variable: "--font-ibm-plex-sans",
+  weight: '300',
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-ibm-plex-sans',
 });
 
 const ibmPlexSerif = IBM_Plex_Serif({
-  weight: "300",
-  display: "swap",
-  subsets: ["latin"],
-  variable: "--font-ibm-plex-serif",
+  weight: '300',
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-ibm-plex-serif',
 });
 
 export const metadata: Metadata = {
-  title: "Dhison P.",
-  description: "Here you learn more about me and what I do.",
+  title: 'Dhison P.',
+  description: 'Here you learn more about me and what I do.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={`${ibmPlexSans.variable} ${ibmPlexSerif.variable}`}
-    >
+    <html lang="en" className={`${ibmPlexSans.variable} ${ibmPlexSerif.variable}`}>
       <body>{children}</body>
     </html>
   );
