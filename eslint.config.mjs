4bcfa742e55d822e2486bb48e250f37f22fd1e2a
Dhison <dhisonp@gmail.com>
2025-05-16 14:10:07 -0400

    chore: adjust eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,9 @@ import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+);
