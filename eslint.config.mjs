c543117d3cc4c325bf6422051f9a3018209cd5d5
Dhison <dhisonp@gmail.com>
2025-05-16 13:57:23 -0400

    chore: add eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier);
