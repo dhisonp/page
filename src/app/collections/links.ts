c96ee5fb15bcdbb0240419b3b8ae2f3c80df2026
Dhison <dhisonp@gmail.com>
2025-05-17 21:17:46 -0400

    chore: remove excessive typing for simplification

--- src/app/collections/links.ts
+++ src/app/collections/links.ts
@@ -1,16 +1,14 @@
-import { Links } from '@/app/types/links';
-
-const links: Links = {
+const links = {
   notion: 'https://dhisonp.notion.site/Dhison-Padma-Software-Engineer-1e147536687b4186b4389e5fbb4b8298',
   dhisvnco: 'https://dhisvn.co',
   ig_dhisvn: 'https://instagram.com/dhisvn',
   ig_dhisonp: 'https://instagram.com/dhisonp',
   youtube: 'https://www.youtube.com/@dhisonpadma',
   behance: 'https://www.behance.net/dhisvn',
-  // tiktok: "https://www.tiktok.com/@dhisonp",
-  tiktok: 'https://www.tiktok.com/@dsvn._',
+  tiktok: 'https://www.tiktok.com/@dhisonpadma',
   github: 'https://github.com/dhisonp',
   linkedin: 'https://www.linkedin.com/in/dhisonp/',
+  twitter: 'https://x.com/DhisonPadma',
 };
 
 export default links;
